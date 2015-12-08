451542a8e8171e169eb8eee04f6b5d01dea5589c
Anthony Ross <amsross@gmail.com>
2015-12-08 17:39:18 -0500

    spawner: use lengths correctly

--- dist/spawner.js
+++ dist/spawner.js
@@ -1,7 +1,7 @@
 var incrementor = 0,
     conquistadors, guards, harvesters, builders,
     MAX_BUILDERS = 2,
-    MAX_HARVESTERS = 8,
+    MAX_HARVESTERS = 6,
     MAX_CONQUISTADORS = 1,
     MAX_GUARDS = 2
     ;
@@ -21,23 +21,31 @@ module.exports = function() {
     return Game.creeps[creep].memory.role === 'builder';
   });
 
-  if (Game.spawns.Spawn1.energy >= 200) {
-    if ( conquistadors <= MAX_CONQUISTADORS )
-      Game.spawns.Spawn1.createCreep( [MOVE, WORK, CARRY, CARRY], 'Conquistador' + incrementor++, {role: 'conquistador'} );
-  }
-
-  if (Game.spawns.Spawn1.energy >= 300) {
+  if ((Game.spawns.Spawn1 || {}).energy >= 300) {
     if ( builders.length >= (harvesters.length / 4) || !Game.rooms.sim.find(FIND_CONSTRUCTION_SITES).length ) {
-      if ( harvesters <= MAX_HARVESTERS )
-        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester' + incrementor++, {role: 'harvester'} );
+      if ( harvesters.length < MAX_HARVESTERS ) {
+        incrementor++;
+        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester' + incrementor, {role: 'harvester'} );
+      }
     } else {
-      if ( builders <= MAX_BUILDERS )
-        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + incrementor++, {role: 'builder'} );
+      if ( builders.length < MAX_BUILDERS ) {
+        incrementor++;
+        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + incrementor, {role: 'builder'} );
+      }
     }
   }
 
-  if (Game.spawns.Spawn1.energy >= 180) {
-    if ( guards <= MAX_GUARDS )
-      Game.spawns.Spawn1.createCreep( [MOVE, MOVE, ATTACK], 'Guard' + incrementor++, {role: 'guard'} );
+  if ((Game.spawns.Spawn1 || {}).energy >= 200) {
+    if ( conquistadors.length < MAX_CONQUISTADORS && harvesters.length == (MAX_HARVESTERS / 2)) {
+      incrementor++;
+      Game.spawns.Spawn1.createCreep( [MOVE, WORK, CARRY, CARRY], 'Conquistador' + incrementor, {role: 'conquistador'} );
+    }
+  }
+
+  if ((Game.spawns.Spawn1 || {}).energy >= 180) {
+    if ( guards.length < MAX_GUARDS ) {
+      incrementor++;
+      Game.spawns.Spawn1.createCreep( [MOVE, MOVE, ATTACK], 'Guard' + incrementor, {role: 'guard'} );
+    }
   }
 };
