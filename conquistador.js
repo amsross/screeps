b506520e03380b9d348f705129711ae193d02736
Anthony Ross <amsross@gmail.com>
2015-12-08 16:38:03 -0500

    conquistador: comments

--- conquistador.js
+++ conquistador.js
@@ -1,13 +1,16 @@
 
 module.exports = function(creep) {
+
   if (creep.room.controller) {
 
+    // capture a controller
     if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
       return creep.moveTo(creep.room.controller);
     }
 
+    // upgrade the controller
     if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller);
+      return creep.moveTo(creep.room.controller);
     }
   }
 };
