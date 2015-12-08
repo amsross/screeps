45f097a41cc2af313fb47cf0bf27f07352292f5c
Anthony Ross <amsross@gmail.com>
2015-12-08 16:37:51 -0500

    builder: comments

--- builder.js
+++ builder.js
@@ -1,14 +1,16 @@
 
 module.exports = function (creep) {
+
   if (creep.carry.energy == 0) {
+    // get more energy
     if (Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(Game.spawns.Spawn1);				
-    }
+      creep.moveTo(Game.spawns.Spawn1);
   } else {
+    // build something
     var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
     if (targets.length) {
       if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0]);					
+        creep.moveTo(targets[0]);
       }
     }
   }
