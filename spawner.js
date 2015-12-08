d10eb0a621d8b46b2785b10e256d7fe477c11d7f
Anthony Ross <amsross@gmail.com>
2015-12-08 16:38:24 -0500

    spawner: max units per role

--- spawner.js
+++ spawner.js
@@ -1,5 +1,9 @@
 var incrementor = 0,
     conquistadors, guards, harvesters, builders
+    MAX_BUILDERS = 2,
+    MAX_HARVESTERS = 8,
+    MAX_CONQUISTADORS = 1,
+    MAX_GUARDS = 2
     ;
 
 module.exports = function() {
@@ -18,16 +22,22 @@ module.exports = function() {
   });
 
   if (Game.spawns.Spawn1.energy >= 200) {
-    if ( !conquistadors.length ) {
+    if ( conquistadors <= MAX_CONQUISTADORS )
       Game.spawns.Spawn1.createCreep( [MOVE, WORK, CARRY, CARRY], 'Conquistador' + incrementor++, {role: 'conquistador'} );
-    }
   }
 
   if (Game.spawns.Spawn1.energy >= 300) {
     if ( builders.length >= (harvesters.length / 4) || !Game.rooms.sim.find(FIND_CONSTRUCTION_SITES).length ) {
-      Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester' + incrementor++, {role: 'harvester'} );
+      if ( harvesters <= MAX_HARVESTERS )
+        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester' + incrementor++, {role: 'harvester'} );
     } else {
-      Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + incrementor++, {role: 'builder'} );
+      if ( builders <= MAX_BUILDERS )
+        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + incrementor++, {role: 'builder'} );
     }
+  }
+
+  if (Game.spawns.Spawn1.energy >= 180) {
+    if ( guards <= MAX_GUARDS )
+      Game.spawns.Spawn1.createCreep( [MOVE, MOVE, ATTACK], 'Guard' + incrementor++, {role: 'guard'} );
   }
 };
