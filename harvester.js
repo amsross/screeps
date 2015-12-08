11825e3b74fa4f162ed827877ab15697476762f7
Anthony Ross <amsross@gmail.com>
2015-12-08 16:38:17 -0500

    harvest: spacing

--- harvester.js
+++ harvester.js
@@ -5,10 +5,10 @@ module.exports = function (creep) {
     var sources = creep.room.find(FIND_SOURCES);
     if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
       creep.moveTo(sources[0]);
-    }			
+    }
   } else {
     if (creep.transferEnergy(Game.spawns.Spawn1) == ERR_NOT_IN_RANGE) {
       creep.moveTo(Game.spawns.Spawn1);
-    }			
+    }
   }
 }
