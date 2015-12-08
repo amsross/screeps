d6fd5f2f2dd15f0453db2da55f7122957a80e906
Anthony Ross <amsross@gmail.com>
2015-12-08 17:38:51 -0500

    guard: don't attack source keeper

--- dist/guard.js
+++ dist/guard.js
@@ -1,10 +1,13 @@
 
 module.exports = function(creep) {
   var targets = creep.room.find(FIND_HOSTILE_CREEPS);
+  targets = targets.filter(function(target) {
+    return target.owner.username !== 'Source Keeper';
+  });
 
   if (targets.length) {
     if (creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(targets[0]);		
+      creep.moveTo(targets[0]);
     }
   }
 }
