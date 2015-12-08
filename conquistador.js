5a6d72bd3c59cf1e171c3abe629d4d9aea19b486
Anthony Ross <amsross@gmail.com>
2015-12-08 16:13:56 -0500

    conquistador: upgrade controller

--- conquistador.js
+++ conquistador.js
@@ -1,8 +1,13 @@
 
 module.exports = function(creep) {
   if (creep.room.controller) {
+
     if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(creep.room.controller);
+      return creep.moveTo(creep.room.controller);
+    }
+
+    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(creep.room.controller);
     }
   }
 };
