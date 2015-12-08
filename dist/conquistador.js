4a9a50e0a388454df84481d5909baf2845a71237
Anthony Ross <amsross@gmail.com>
2015-12-08 17:11:14 -0500

    conquistador: get more energy

--- dist/conquistador.js
+++ dist/conquistador.js
@@ -3,14 +3,23 @@ module.exports = function(creep) {
 
   if (creep.room.controller) {
 
-    // capture a controller
-    if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-      return creep.moveTo(creep.room.controller);
-    }
+    if (creep.carry.energy == 0) {
 
-    // upgrade the controller
-    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-      return creep.moveTo(creep.room.controller);
+      // get more energy
+      if (Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.spawns.Spawn1);
+      }
+    } else {
+
+      // capture a controller
+      if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(creep.room.controller);
+      }
+
+      // upgrade the controller
+      if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(creep.room.controller);
+      }
     }
   }
 };
