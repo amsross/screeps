4e02cee6ec15f25e3d18b6184c4713325901d081
Anthony Ross <amsross@gmail.com>
2015-12-14 08:55:47 -0500

    builder: add repair routine

--- dist/builder.js
+++ dist/builder.js
@@ -7,11 +7,25 @@ module.exports = function (creep) {
       creep.moveTo(Game.spawns.Spawn1);
     }
   } else {
-    // build something
-    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    // repair something
+    var targets = creep.room.find(FIND_MY_STRUCTURES, {
+      filter: function(target) {
+        return target.hits < target.hitsMax;
+      }
+    });
     if (targets.length) {
-      if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0]);
+      var target = creep.pos.findClosestByRange(targets);
+      if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target);
+      }
+    } else {
+      // build something
+      var targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+      if (targets) {
+        var target = creep.pos.findClosestByRange(targets);
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
+        }
       }
     }
   }
