50e871e10c17e438a706e4e9f5d06f1810e55754
Anthony Ross <amsross@gmail.com>
2015-12-14 08:56:30 -0500

    spawner: add new roles and base spawning on population ratio

--- dist/spawner.js
+++ dist/spawner.js
@@ -1,58 +1,73 @@
-var incrementor = 0,
-    conquistadors, guards, harvesters, builders,
-    MAX_BUILDERS = 8,
-    MAX_HARVESTERS = 8,
-    MAX_CONQUISTADORS = 1,
-    MAX_GUARDS = 2
-    ;
 
 module.exports = function() {
 
-  guards = Object.keys(Game.creeps).filter(function(creep) {
-    return Game.creeps[creep].memory.role === 'guard';
-  });
-  conquistadors = Object.keys(Game.creeps).filter(function(creep) {
-    return Game.creeps[creep].memory.role === 'conquistador';
-  });
-  harvesters = Object.keys(Game.creeps).filter(function(creep) {
-    return Game.creeps[creep].memory.role === 'harvester';
-  });
-  builders = Object.keys(Game.creeps).filter(function(creep) {
-    return Game.creeps[creep].memory.role === 'builder';
-  });
-
   if ((Game.spawns.Spawn1 || {}).energy >= 300) {
-    if ( (builders.length / harvesters.length) > 0.33 || !Game.rooms.sim.find(FIND_CONSTRUCTION_SITES).length ) {
-      if ( harvesters.length < MAX_HARVESTERS ) {
-        incrementor++;
-        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester' + incrementor, {role: 'harvester'} );
+
+    if ( false && Game.spawns.Spawn1.room.find(FIND_HOSTILE_CREEPS, {
+      filter: function(target) {
+        return target.owner.username !== 'Source Keeper';
       }
+    }).length ) {
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'Guard' + Date.now(), {role: 'guard'} );
+    }
+
+    if ( !Object.keys( Game.creeps ).length ) {
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'harvester' + Date.now(), {role: 'harvester'} );
+    } else if ( spawnDeciderByRole( "miner" ) ) {
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, MOVE, MOVE], 'miner' + Date.now(), {role: 'miner'} );
+    } else if ( spawnDeciderByRole( "scavenger" ) ) {
+      return Game.spawns.Spawn1.createCreep( [CARRY, CARRY, CARRY, MOVE, MOVE, MOVE], 'scavenger' + Date.now(), {role: 'scavenger'} );
+    } else if ( spawnDeciderByRole( "guard" ) ) {
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'guard' + Date.now(), {role: 'guard'} );
+    } else if ( spawnDeciderByRole( "healer" ) ) {
+      return Game.spawns.Spawn1.createCreep( [MOVE, HEAL], 'healer' + Date.now(), {role: 'healer'} );
+    } else if ( spawnDeciderByRole( "archer" ) ) {
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, RANGED_ATTACK], 'archer' + Date.now(), {role: 'archer'} );
+    } else if ( spawnDeciderByRole( "conquistador" ) ) {
+      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + Date.now(), {role: 'conquistador'} );
+    } else if ( spawnDeciderByRole( "builder" ) && Game.rooms.sim.find(FIND_CONSTRUCTION_SITES).length ) {
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'builder' + Date.now(), {role: 'builder'} );
     } else {
-      if ( builders.length < MAX_BUILDERS ) {
-        incrementor++;
-        Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + incrementor, {role: 'builder'} );
-      }
-    }
-  }
-
-  if ((Game.spawns.Spawn1 || {}).energy >= 200) {
-    if ( conquistadors.length < MAX_CONQUISTADORS && harvesters.length == (MAX_HARVESTERS / 2)) {
-      incrementor++;
-      Game.spawns.Spawn1.createCreep( [MOVE, WORK, CARRY, CARRY], 'Conquistador' + incrementor, {role: 'conquistador'} );
-    }
-  }
-
-  if ((Game.spawns.Spawn1 || {}).energy >= 270) {
-    if ( guards.length < MAX_GUARDS * 4 ) {
-      incrementor++;
-      Game.spawns.Spawn1.createCreep( [MOVE, MOVE, ATTACK, ATTACK, TOUGH, TOUGH], 'Guard' + incrementor, {role: 'guard'} );
-    }
-  }
-
-  if ((Game.spawns.Spawn1 || {}).energy >= 180) {
-    if ( guards.length < MAX_GUARDS ) {
-      incrementor++;
-      Game.spawns.Spawn1.createCreep( [MOVE, MOVE, ATTACK], 'Guard' + incrementor, {role: 'guard'} );
+      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + Date.now(), {role: 'conquistador'} );
     }
   }
 };
+
+function countCreepsByRole( role ) {
+  return Object.keys(Game.creeps).filter(function(creep) {
+    return Game.creeps[creep].memory.role === role;
+  });
+}
+
+function ratioCreepsByRole( role ) {
+  var count = countCreepsByRole( role ).length || 0;
+  return count / (Object.keys(Game.creeps).length || 1);
+}
+
+function spawnDeciderByRole( role ) {
+  var RATIO = {
+    guard: .35, // 7 @20
+    scavenger: .25, // 5 @20
+    archer: .15,  // 3 @20
+    miner: .10, // 2 @20
+    healer: .05, // 1 @20
+    builder: .05, // 1 @20
+    conquistador: .05  // 1 @20
+  };
+
+  return ratioCreepsByRole( role ) <= RATIO[role];
+}
+
+/*
+ * Ratio distrbution over population 100
+  g g g g g g g g g g
+  g g g g g g g g g g
+  g g g g g g g g g g
+  g g g g g s s s s s
+  s s s s s s s s s s
+  s s s s s s s s s s
+  a a a a a a a a a a
+  a a a a a m m m m m
+  m m m m m h h h h h
+  b b b b b c c c c c
+  */
