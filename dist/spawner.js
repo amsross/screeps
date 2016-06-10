e873c034860bef18a0bcfc6c3b1fc4995165e273
Matt Ross <matt@mattross.io>
2016-06-10 16:27:09 -0400

    Use single date instance

--- dist/spawner.js
+++ dist/spawner.js
@@ -3,32 +3,34 @@ module.exports = function() {
 
   if ((Game.spawns.Spawn1 || {}).energy >= 300) {
 
+    var now = Date.now()
+
     if ( false && Game.spawns.Spawn1.room.find(FIND_HOSTILE_CREEPS, {
       filter: function(target) {
         return target.owner.username !== 'Source Keeper';
       }
     }).length ) {
-      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'Guard' + Date.now(), {role: 'guard'} );
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'Guard' + now, {role: 'guard'} );
     }
 
     if ( !Object.keys( Game.creeps ).length ) {
-      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'harvester' + Date.now(), {role: 'harvester'} );
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'harvester' + now, {role: 'harvester'} );
     } else if ( spawnDeciderByRole( "miner" ) ) {
-      return Game.spawns.Spawn1.createCreep( [WORK, WORK, MOVE, MOVE], 'miner' + Date.now(), {role: 'miner'} );
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, MOVE, MOVE], 'miner' + now, {role: 'miner'} );
     } else if ( spawnDeciderByRole( "scavenger" ) ) {
-      return Game.spawns.Spawn1.createCreep( [CARRY, CARRY, CARRY, MOVE, MOVE, MOVE], 'scavenger' + Date.now(), {role: 'scavenger'} );
+      return Game.spawns.Spawn1.createCreep( [CARRY, CARRY, CARRY, MOVE, MOVE, MOVE], 'scavenger' + now, {role: 'scavenger'} );
     } else if ( spawnDeciderByRole( "guard" ) ) {
-      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'guard' + Date.now(), {role: 'guard'} );
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, MOVE, ATTACK, MOVE, ATTACK], 'guard' + now, {role: 'guard'} );
     } else if ( spawnDeciderByRole( "healer" ) ) {
-      return Game.spawns.Spawn1.createCreep( [MOVE, HEAL], 'healer' + Date.now(), {role: 'healer'} );
+      return Game.spawns.Spawn1.createCreep( [MOVE, HEAL], 'healer' + now, {role: 'healer'} );
     } else if ( spawnDeciderByRole( "archer" ) ) {
-      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, RANGED_ATTACK], 'archer' + Date.now(), {role: 'archer'} );
+      return Game.spawns.Spawn1.createCreep( [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, RANGED_ATTACK], 'archer' + now, {role: 'archer'} );
     } else if ( spawnDeciderByRole( "conquistador" ) ) {
-      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + Date.now(), {role: 'conquistador'} );
+      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + now, {role: 'conquistador'} );
     } else if ( spawnDeciderByRole( "builder" ) && Game.rooms.sim.find(FIND_CONSTRUCTION_SITES).length ) {
-      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'builder' + Date.now(), {role: 'builder'} );
+      return Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'builder' + now, {role: 'builder'} );
     } else {
-      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + Date.now(), {role: 'conquistador'} );
+      return Game.spawns.Spawn1.createCreep( [MOVE, WORK, WORK, CARRY], 'conquistador' + now, {role: 'conquistador'} );
     }
   }
 };
